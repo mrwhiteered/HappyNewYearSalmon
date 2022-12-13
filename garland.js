45ad7dbdea523da49d69875eac2e94d9a1c5f8f1
Daniil <68585455+mrwhiteered@users.noreply.github.com>
2022-12-13 19:19:41 +0300

    g13
    g13 -filter

--- garland.js
+++ garland.js
@@ -5,15 +5,15 @@ function garland(onlyFill){
 
     // Достаем элементы с HTML
         let changeFill = document.getElementById('path1');
-        let changeFilter = document.getElementById('fon1');
+        // let// changeFilter = document.getElementById('fon1');
         let changeFill2 = document.getElementById('path2');
-        let changeFilter2 = document.getElementById('fon2');
+        // let// changeFilter2 = document.getElementById('fon2');
         let changeFill3 = document.getElementById('path3');
-        let changeFilter3 = document.getElementById('fon3');
+        // let// changeFilter3 = document.getElementById('fon3');
         let changeFill4 = document.getElementById('path4');
-        let changeFilter4 = document.getElementById('fon4');
+        // let// changeFilter4 = document.getElementById('fon4');
         let changeFill5 = document.getElementById('path5');
-        let changeFilter5 = document.getElementById('fon5');
+        // let// changeFilter5 = document.getElementById('fon5');
     
     
     // Достаем объект со стилями 
@@ -30,80 +30,80 @@ function garland(onlyFill){
         if (makeString === '"rgb(255, 0, 0)"'){ //красный на желтый 
             
             changeFill.style.fill  = "yellow";
-            changeFilter.style.filter = "drop-shadow(1px 1px 10px yellow)";
+           // changeFilter.style.filter = "drop-shadow(1px 1px 10px yellow)";
             changeFill2.style.fill  = "purple";
-            changeFilter2.style.filter = "drop-shadow(1px 1px 10px purple)";
+           // changeFilter2.style.filter = "drop-shadow(1px 1px 10px purple)";
             changeFill3.style.fill  = "blue";
-            changeFilter3.style.filter = "drop-shadow(1px 1px 10px blue)";
+           // changeFilter3.style.filter = "drop-shadow(1px 1px 10px blue)";
             changeFill4.style.fill  = "rgb(0, 255, 0)";
-            changeFilter4.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
+           // changeFilter4.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
             changeFill5.style.fill  = "red";
-            changeFilter5.style.filter = "drop-shadow(1px 1px 10px red)";
+           // changeFilter5.style.filter = "drop-shadow(1px 1px 10px red)";
 
         }  
         if  (makeString ==='"rgb(255, 255, 0)"') // желтый на фиолетовый 
         {
             changeFill5.style.fill  = "yellow";
-            changeFilter5.style.filter = "drop-shadow(1px 1px 10px yellow)";
+           // changeFilter5.style.filter = "drop-shadow(1px 1px 10px yellow)";
             changeFill.style.fill  = "purple";
-            changeFilter.style.filter = "drop-shadow(1px 1px 10px purple)";
+           // changeFilter.style.filter = "drop-shadow(1px 1px 10px purple)";
             changeFill2.style.fill  = "blue";
-            changeFilter2.style.filter = "drop-shadow(1px 1px 10px blue)";
+           // changeFilter2.style.filter = "drop-shadow(1px 1px 10px blue)";
             changeFill3.style.fill  = "rgb(0, 255, 0)";
-            changeFilter3.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
+           // changeFilter3.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
             changeFill4.style.fill  = "red";
-            changeFilter4.style.filter = "drop-shadow(1px 1px 10px red)";
+           // changeFilter4.style.filter = "drop-shadow(1px 1px 10px red)";
         } 
         if (makeString === '"rgb(128, 0, 128)"') // фиолетовый в синий 
         {
             changeFill4.style.fill  = "yellow";
-            changeFilter4.style.filter = "drop-shadow(1px 1px 10px yellow)";
+           // changeFilter4.style.filter = "drop-shadow(1px 1px 10px yellow)";
 
             changeFill5.style.fill  = "purple";
-            changeFilter5.style.filter = "drop-shadow(1px 1px 10px purple)";
+           // changeFilter5.style.filter = "drop-shadow(1px 1px 10px purple)";
 
             changeFill.style.fill  = "blue";
-            changeFilter.style.filter = "drop-shadow(1px 1px 10px blue)";
+           // changeFilter.style.filter = "drop-shadow(1px 1px 10px blue)";
 
             changeFill2.style.fill  = "rgb(0, 255, 0)";
-            changeFilter2.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
+           // changeFilter2.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
 
             changeFill3.style.fill  = "red";
-            changeFilter3.style.filter = "drop-shadow(1px 1px 10px red)";
+           // changeFilter3.style.filter = "drop-shadow(1px 1px 10px red)";
         }
         if (makeString === '"rgb(0, 0, 255)"') // синий в зеленый 
         {
             changeFill3.style.fill  = "yellow";
-            changeFilter3.style.filter = "drop-shadow(1px 1px 10px yellow)";
+           // changeFilter3.style.filter = "drop-shadow(1px 1px 10px yellow)";
 
             changeFill4.style.fill  = "purple";
-            changeFilter4.style.filter = "drop-shadow(1px 1px 10px purple)";
+           // changeFilter4.style.filter = "drop-shadow(1px 1px 10px purple)";
 
             changeFill5.style.fill  = "blue";
-            changeFilter5.style.filter = "drop-shadow(1px 1px 10px blue)";
+           // changeFilter5.style.filter = "drop-shadow(1px 1px 10px blue)";
 
             changeFill.style.fill  = "rgb(0, 255, 0)";
-            changeFilter.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
+           // changeFilter.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
 
             changeFill2.style.fill  = "red";
-            changeFilter2.style.filter = "drop-shadow(1px 1px 10px red)";
+           // changeFilter2.style.filter = "drop-shadow(1px 1px 10px red)";
         }
         if (makeString === '"rgb(0, 255, 0)"')
         {
             changeFill2.style.fill  = "yellow";
-            changeFilter2.style.filter = "drop-shadow(1px 1px 10px yellow)";
+           // changeFilter2.style.filter = "drop-shadow(1px 1px 10px yellow)";
 
             changeFill3.style.fill  = "purple";
-            changeFilter3.style.filter = "drop-shadow(1px 1px 10px purple)";
+           // changeFilter3.style.filter = "drop-shadow(1px 1px 10px purple)";
 
             changeFill4.style.fill  = "blue";
-            changeFilter4.style.filter = "drop-shadow(1px 1px 10px blue)";
+           // changeFilter4.style.filter = "drop-shadow(1px 1px 10px blue)";
 
             changeFill5.style.fill  = "rgb(0, 255, 0)";
-            changeFilter5.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
+           // changeFilter5.style.filter = "drop-shadow(1px 1px 10px rgb(0, 255, 0))";
 
             changeFill.style.fill  = "red";
-            changeFilter.style.filter = "drop-shadow(1px 1px 10px red)";
+           // changeFilter.style.filter = "drop-shadow(1px 1px 10px red)";
             }
     }
     setInterval(function(onlyFill) {
